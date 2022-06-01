4a68663fb477ad49d17cc107910292632a6370c9
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-06-01 16:52:17 -0400

    added list and arrays

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string stranger = "Another";
+int one = 1;
+
+Console.WriteLine($"Hello, {stranger} {one}");
+
+Console.Write(stranger.IndexOf("n").ToString());
+
+int a = 10, b = 20, c = 40; 
+
+int sum = a + b + c;
+
+double d = 5.0,  e = 2.0; 
+
+double div = d / e;
+// int max = int.M
+
+Console.WriteLine($"The sum is {sum}");
+Console.WriteLine($"What is 70/3 { div }?");
+
+Console.WriteLine($"What is the max of double {double.MaxValue}");
+Console.WriteLine($"What is the max of int {int.MaxValue}");
+
+
+Console.WriteLine("--------------------------------------------------- List -------------------------------");
+
+// int[] arr2 = new int[] {1,2,3,4};
+var firstArr = new int[5] {-69, 46, 10, 1, 100};
+
+firstArr[1] = 20;
+
+
+// firstArr[0] = 100;
+
+for(var i =0; i < firstArr.Count(); i++)
+{
+    Console.WriteLine($"The value at {i} is {firstArr[i]}");
+}
+foreach(var el in firstArr)
+{
+    Console.WriteLine($"The value is {el}");
+}
+
+Console.WriteLine("--------------------------------------------------- List -------------------------------");
+
+List<int> list = new List<int>();
+
+list.AddRange(firstArr);
+list.Add(1000);
+list.Add(1001);
+list.Add(1002);
+
+foreach(var el in list)
+{
+    Console.WriteLine($"The value is {el}");
+}
+
+Console.WriteLine($"The first element is {list[0]}");
+
